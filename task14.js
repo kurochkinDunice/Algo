fc129356bb63cb6e7309b940455357565adf6e40
kurochkinDunice <d.kurochkin@nv.dunice.net>
2023-02-28 09:23:48 +0300

    add: merge sort

--- task14.js
+++ task14.js
@@ -1,42 +1,53 @@
+// O(n log n)
 const arr = [20, 72, 3, 46, 33, 0, 12, 54, -2, 4]            
 
-function Sort(array, start, end) {
-    if (start - end < 2) return;
-    if (start - end == 2) {
-        if (array[start] > array[start + 1]) {
-            let tmp = array[start]
-            array[start] = array[start + 1]
-            array[start + 1] = tmp;
+function Merge(a, low, mid, high) {
+    var b = new Array(high + 1 - low), h, i, j = mid + 1, k, h = low, i = 0;
+    while (h <= mid && j <= high ) { 
+        if (a[h] <= a[j]) { 
+            b[i] = a[h]; h++; 
         }
-        return; 
+        else { 
+            b[i]=a[j]; j++;
+         }
+        i++;
     }
-    
-    Sort(array, start, start + (end - start) / 2);
-    Sort(array, start + (end - start) / 2, end);
-    
-    const newArr = [];
-    let b1 = start;
-    let el = start + (end - start) / 2;
-    let b2 = el
-
-    while (newArr.length < end - start) {
-        if (b1 >= el || (b2 < end && array[b2] <= array[b1])) {
-            newArr.push(array[b2]);
-            ++b2;
-        } else {
-            newArr.push(array[b1]);
-            ++b1;
-        }
+    if (h > mid) { 
+        for (k = j; k <= high; k++) { 
+            b[i] = a[k]; i++; 
+        } 
+    }
+    else { 
+        for (k = h; k <= mid; k++) {  
+            b[i] = a[k]; i++; } 
     }
 
-    for (let i = start; i < end; ++i) {
-        array[i] = newArr[i - start];
+    for (k = 0; k <= high - low; k++) { 
+        a[k + low] = b[k];
     }
+
+    return a;
+}
+
+function MergeSort(A) {
+
+    function merge_sort(a, low, high) { 
+        if (low < high) { 
+            let mid = Math.floor((low + high) / 2);
+            merge_sort(a, low, mid);
+            merge_sort(a, mid + 1, high);
+            Merge(a, low, mid, high);
+        }
+     }
+
+    let n = A.length;
+    merge_sort(A, 0, n-1);
+    return A;
 }
 
 console.time('TIME')
 
-Sort(arr, 0, arr.length - 1)
+MergeSort(arr)
 
 console.timeEnd('TIME')
 
