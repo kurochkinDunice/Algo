0682115dd0040b0b49e6a851059ee8feff52c132
kurochkinDunice <d.kurochkin@nv.dunice.net>
2023-03-23 16:14:58 +0300

    add: task16

---
+++ task16.js
@@ -0,0 +1,16 @@
+function sumDigit(num) {
+    let sum = 0n
+    do {
+        sum += num % 10n
+        num /= 10n
+    } while (num > 0)
+    return sum
+}
+
+console.log(sumDigit(BigInt(Math.pow(2, 1000))));
+
+console.time('TIME');
+let ans = sumDigit(BigInt(Math.pow(2, 1000)));
+console.timeEnd('TIME');
+
+console.log(ans);
